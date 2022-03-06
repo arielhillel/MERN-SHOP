42d59045c058e8c950b44c7659784e33795efec8
Ariel Hillel <arielhillel77@gmail.com>
2022-03-06 17:51:36 +0200

    products page layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Products />
+    </>
+  );
+};
+export default App;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Products.css";
+import Product from "../Product/Product";
+
+const Products = () => {
+  return (
+    <section className="products">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+};
+
+export default Products;
